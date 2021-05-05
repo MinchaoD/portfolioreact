98c4c1ab160d50aa5c56365a9b4e8dd20f550152
chaohg <chaohg@hotmail.com>
2021-05-04 23:43:15 -0700

    add animation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Main() {
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
+import Typical from 'react-typical'
+
+class AnimationText extends Component {
+  render () {
     return (
-        <div>
-            
-        </div>
+        <div className="text">
+            <Typical
+                steps={['Javascript', 1000, 'React', 1000, 'React Native', 500, 'Moogodb', 1000]}
+                loop={Infinity}
+                wrapper="b"
+            />
+      </div>
     )
+  }
 }
+function Presenter () {
+    const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+    const FadeUp = batch(Fade(), Move(), Sticky());
+  
+    return (
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={ZoomInScrollOut}>
+            <span style={{ fontSize: "3em" }}>I'm Minchao Duclos </span> <br /> 
+            <span style={{ fontSize: "3em" }} >I'm a Developer & Designer</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <Animator animation={FadeUp}>
+            <AnimationText />
+          </Animator>
+        </ScrollPage>
+    </ScrollContainer>
+    );
+  };
 
-export default Main
+
+ 
+ class Main extends Component {
+     render() {
+         return (
+            <div style={{ 
+                backgroundImage: "url(/images/backgroundimage.jpg)" 
+              }}>
+                <Presenter />
+            </div>
+         )
+     }
+ }
+ 
+
+export default Main;
