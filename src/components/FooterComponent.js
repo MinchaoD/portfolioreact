b852313e4152b4d29888c978dc27ca53352d756d
chaohg <chaohg@hotmail.com>
2021-05-07 00:17:03 -0700

    update footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -7,21 +7,21 @@ class Footer extends Component {
     
         return (
             <React.Fragment>
-                <footer className="site-footer">
+                <footer className="site-footer container">
                  
-                        <div class="row row-content-footer text-center">
-                            <div class="col-4">
-                                <a role="button" class="btn btn-dark btn-lg"  href="tel:+12063838906"><i class="fa fa-phone-square fa-lg"></i> Phone ： 1-206-383-8906 </a><br />
+                        <div className="row text-center">
+                            <div className="col-4">
+                                <a role="button" className="btn btn-dark btn-lg"  href="tel:+12063838906"><i class="fa fa-phone-square fa-lg"></i> Phone ： 1-206-383-8906 </a><br />
                             </div>
-                            <div class="col-4">
-                                <p> made with <i class="fa fa-heart"></i> in Seattle</p>
+                            <div className="col-4">
+                                <p> made with <i className="fa fa-heart"></i> in Seattle</p>
                             </div>
-                            <div class="col-4 ">
-                                <a role="button" class="btn btn-dark btn-lg"  href="mailto: chaohg@hotmail.com"><i class="fa fa-envelope-square fa-lg"></i> Email :  chaohg@hotmail.com </a>
+                            <div className="col-4">
+                                <a role="button" className="btn btn-dark btn-lg"  href="mailto: chaohg@hotmail.com"><i class="fa fa-envelope-square fa-lg"></i> Email :  chaohg@hotmail.com </a>
                             </div>
                         </div>
-                        <div class="row text-center">
-                            <div class="col">
+                        <div className="row text-center">
+                            <div className="col">
                             <p>copyright ©  {year}</p>
                             </div>
                         </div>
