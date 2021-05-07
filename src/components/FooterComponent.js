bc759b6281f6b7ed127840e54d562bc6aa2303b1
chaohg <chaohg@hotmail.com>
2021-05-06 22:55:29 -0700

    replace with cdn links

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react'
 
 class Footer extends Component {
     render() {
+        const date = new Date(); 
+        const year = date.getFullYear();
+    
         return (
             <React.Fragment>
                 <footer className="site-footer">
                  
-                    <div class="container">
                         <div class="row row-content-footer text-center">
                             <div class="col-4">
-                                <a role="button" class="btn btn-dark"  href="tel:+12063838906"><i class="fa fa-phone-square fa-lg"></i> Phone ： 1-206-383-8906 </a><br />
+                                <a role="button" class="btn btn-dark btn-lg"  href="tel:+12063838906"><i class="fa fa-phone-square fa-lg"></i> Phone ： 1-206-383-8906 </a><br />
                             </div>
                             <div class="col-4">
-                                <h5> made with <i class="fa fa-heart"></i> in Seattle</h5>
+                                <p> made with <i class="fa fa-heart"></i> in Seattle</p>
                             </div>
                             <div class="col-4 ">
-                                <a role="button" class="btn btn-dark"  href="mailto: chaohg@hotmail.com"><i class="fa fa-envelope-square fa-lg"></i> Email :  chaohg@hotmail.com </a>
+                                <a role="button" class="btn btn-dark btn-lg"  href="mailto: chaohg@hotmail.com"><i class="fa fa-envelope-square fa-lg"></i> Email :  chaohg@hotmail.com </a>
                             </div>
                         </div>
                         <div class="row text-center">
                             <div class="col">
-                            <p id="year"></p>
+                            <p>copyright ©  {year}</p>
                             </div>
                         </div>
-                    </div>
                 </footer>
             </React.Fragment>
+
+
+
             
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,56 +6,79 @@ import { Card, CardImg, CardTitle, CardText, CardBody, Button, CardDeck} from 'r
 import Footer from './FooterComponent';
 
 
-
-
 function Skills() {
     return (
-    <div>
-         <span style={{ fontSize: "3em" }} align="center">I'm a web developer and good at html, css, javascript </span>
+    <div className="container" align="center">
+         <span className="title" style={{ fontSize: "3em" }} >About me </span>
+         <br/><br /> <br />
+         <span style={{ fontSize: "2em" }}>I'm a Front End / Full Stack developer and I love to code. I enjoy the happiness of building some cool and user friendly websites, apps and games. My skills include, but are not limited to: Html, Css, BootStrap, Javascript, ReactJS, Redux, Rest API, React Native, NodeJS, Express, MongoDB </span>
+       
     </div>
     )
     
 }  
 
 function Projects() {
+   
+
     return (
+        <div className="container" align="center">
+        <span className="title" style={{ fontSize: "3em" }} >Projects </span>
+        <br/><br /> <br />
         <div className ="row justify-content-center">
         <div className = "col ">
             <CardDeck>
                 <Card>
-                        <CardImg  height="400"  />
-                        <CardBody className="cardinfo" >
+                        <CardImg  height="400"  src="/assets/images/project-hawaii.png" alt="image"/>
+                        <CardBody>
+                        
                             <CardTitle>Hawaii Travel</CardTitle>
+                      
                             <CardText>
-                                <span>This project I used mostly bootstrap to create my own designed travel website.</span>
+                                <span> For this project I used mostly Bootstrap and Css to create a Hawaii travel website. I focused on layout design, user interaction and code cleanup. </span>
                             </CardText>
-                            <a target="_blank" href="https://github.com/chaohg/traveltoHawaii" style={{textDecoration : "none"}}> <Button size="lg"  className="btn-project">Demo</Button></a>
+                            <br/>
+                            <a target="_blank" href="https://github.com/chaohg/traveltoHawaii" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float:'left'}}>Demo</Button></a>
                            
-                            <a target="_blank" href="https://github.com/chaohg/traveltoHawaii" style={{textDecoration : "none"}}> <Button size="lg"  className="btn-project" style={{float: 'right'}}>Code</Button></a>
+                            <a target="_blank" href="https://github.com/chaohg/traveltoHawaii" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float: 'right'}}>Code</Button></a>
                         </CardBody>
                 </Card>
                 <Card>
-                        <CardImg  height="400"  />
-                        <CardBody className="cardinfo" >
-                            <CardTitle>Hawaii Travel</CardTitle>
+                        <CardImg  height="400"  src="/assets/images/project-house.png" alt="image"/>
+                        <CardBody>
+                        
+                            <CardTitle>House Search</CardTitle>
+                      
                             <CardText>
-                                <span>This project I used mostly bootstrap to create my own designed travel website.</span>
+                                <span>I used ReactJS to link a mockup mini dataset with the website. The same information and layout for each house were rendered with no-repeat code.</span>
                             </CardText>
+                            <br/>
+                            <a target="_blank" href="https://github.com/chaohg/realestateapp" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float:'left'}}>Demo</Button></a>
+                           
+                            <a target="_blank" href="https://github.com/chaohg/realestateapp" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float: 'right'}}>Code</Button></a>
                         </CardBody>
                 </Card>
                 <Card>
-                        <CardImg  height="400"  />
-                        <CardBody className="cardinfo" >
-                            <CardTitle>Hawaii Travel</CardTitle>
+                <CardImg  height="400"  src="/assets/images/project-house.png" alt="image"/>
+                        <CardBody>
+                        
+                            <CardTitle>Drum Game</CardTitle>
+                      
                             <CardText>
-                                <span>This project I used mostly bootstrap to create my own designed travel website.</span>
+                                <span> This is a little fun project! I used Vanilla JavaScript to create a Drum Playing Game. The drum can be played by both key press and mouse click. </span>
                             </CardText>
+                            <br />
+                            <a target="_blank" href="https://github.com/chaohg/realestateapp" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float:'left'}}>Demo</Button></a>
+                           
+                            <a target="_blank" href="https://github.com/chaohg/realestateapp" style={{textDecoration : "none"}}> <Button className="btn-project" style={{float: 'right'}}>Code</Button></a>
                         </CardBody>
                 </Card>
                 
             </CardDeck>
         </div>
     </div>
+   </div>
+        
     )
     
 }  
@@ -66,7 +89,7 @@ class AnimationText extends Component {
           <div className="text">
               
               <Typical
-                  steps={['Javascript', 1000, 'ReactJS', 1000, 'React Native', 1000, 'Redux', 1000,'BootStrap', 1000,'Python', 1000,'MoogoDb', 1000]}
+                  steps={['Javascript', 1000, 'ReactJS', 1000, 'React Native', 1000, 'Redux', 1000,'BootStrap', 1000,'Python', 1000,'MongoDb', 1000]}
                   loop={Infinity}
                   wrapper="b"
               />
@@ -74,20 +97,23 @@ class AnimationText extends Component {
       )
     }
   }
+
+
 function Presenter () {
-    const FadeUp = batch(Fade(), Move());
+    const FadeUp = batch(Move());
   
     return (
       <ScrollContainer >
         <ScrollPage page={0} >
           <Animator  animation={FadeUp}>
-              
+            <Zoom>  
             <div align="center" >
-                <span style={{ fontSize: "3em" }} align="center">I'm </span> <span style={{fontSize: "4em", color:"#800000"}}>Minchao Duclos </span> <br /> 
-                <span style={{ fontSize: "3em" }} align="center">a Developer & Designer</span>
+                <span style={{ fontSize: "3em" }}>Hi, I'm </span> <span style={{fontSize: "4em", color:"#800000"}}>Minchao Duclos </span> <br /> 
+                <span style={{ fontSize: "3em" }}>a Developer & Designer</span>
                 <br/><br/><br/>
                 <AnimationText />
             </div>
+            </Zoom>
           </Animator>
         </ScrollPage>
         <ScrollPage page={1}>
